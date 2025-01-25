f80ddc838b12c3886473bc18dde89dafd0c9a1af
Peeranat Thantaletong <porpeeranut@gmail.com>
2025-01-26 00:14:32 +0700

    add reset button to get default setting

--- src/main/java/burp/SettingForm.java
+++ src/main/java/burp/SettingForm.java
@@ -17,6 +17,8 @@ import com.webauthn4j.util.RSAUtil;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Type;
 import java.util.Map;
 
@@ -43,6 +45,7 @@ public class SettingForm {
 	private JRadioButton ES384RadioButton;
 	private JRadioButton ES512RadioButton;
 	private JButton generateButton;
+	private JButton resetButton;
 	private JButton saveButton;
 	private JCheckBox registerClientDataJsonEncodedChkbox;
 	private JCheckBox registerAttestationObjectEncodedChkbox;
@@ -228,6 +231,29 @@ public class SettingForm {
 			settingData.setString("coseKeyJsonString", coseKeyJsonString);
 
 			printSetting(false);
+		});
+
+		resetButton.addActionListener(e -> {
+			registrationUrlField.setText("");
+			registrationClientDataJSONField.setText("\"clientDataJSON\":\"([^\"]+)");
+			registrationAttestationObjectField.setText("\"attestationObject\":\"([^\"]+)");
+
+			registerClientDataJsonEncodedChkbox.setSelected(false);
+			registerAttestationObjectEncodedChkbox.setSelected(false);
+			registerClientDataJsonBase64URLRadBtn.setSelected(true);
+			registerAttestationObjectBase64URLRadBtn.setSelected(true);
+
+			authenticationUrlField.setText("");
+			authenticationClientDataJSONField.setText("\"clientDataJSON\":\"([^\"]+)");
+			authenticationAuthenticatorDataField.setText("\"authenticatorData\":\"([^\"]+)");
+			authenticationSignatureField.setText("\"signature\":\"([^\"]+)");
+
+			authenClientDataJsonEncodedChkbox.setSelected(false);
+			authenAuthenticatorDataEncodedChkbox.setSelected(false);
+			authenSignatureEncodedChkbox.setSelected(false);
+			authenClientDataJsonBase64URLRadBtn.setSelected(true);
+			authenAuthenticatorDataBase64URLRadBtn.setSelected(true);
+			authenSignatureBase64URLRadBtn.setSelected(true);
 		});
 	}
 
@@ -513,15 +539,18 @@ public class SettingForm {
 		authenSignatureBase64URLRadBtn.setText("Base64URL");
 		panel5.add(authenSignatureBase64URLRadBtn, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
 		final JPanel panel6 = new JPanel();
-		panel6.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
+		panel6.setLayout(new GridLayoutManager(2, 3, new Insets(0, 0, 0, 0), -1, -1));
 		mainPanel.add(panel6, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, new Dimension(1000, -1), 0, false));
 		saveButton = new JButton();
 		saveButton.setText("Save");
-		panel6.add(saveButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+		panel6.add(saveButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
 		final Spacer spacer2 = new Spacer();
 		panel6.add(spacer2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
 		final Spacer spacer3 = new Spacer();
-		panel6.add(spacer3, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+		panel6.add(spacer3, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+		resetButton = new JButton();
+		resetButton.setText("Reset");
+		panel6.add(resetButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
 		ButtonGroup buttonGroup;
 		buttonGroup = new ButtonGroup();
 		buttonGroup.add(ES256RadioButton);
